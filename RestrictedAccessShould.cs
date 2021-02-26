3b5a7f1f6f75178e2aecfedc68df34686a041df3
Joao Santana <jfsant2017@gmail.com>
2021-02-26 14:33:27 -0300

    Refactored test cases

--- RestrictedAccessShould.cs
+++ RestrictedAccessShould.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
@@ -6,21 +7,37 @@ using System.Threading.Tasks;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace JwtBearerExample.Tests
 {
-    public class RestrictedAccessShould
+    public class RestrictedAccessShould: IDisposable
     {
-        [Fact]
-        public async Task AllowAccessPublicData()
+        private readonly ITestOutputHelper _output;
+        private readonly HttpClient _apiClient;        
+
+        public RestrictedAccessShould(ITestOutputHelper output)
         {
+            _output = output;
             HttpClientHandler clientHandler = new HttpClientHandler();
             clientHandler.ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; };
+            _apiClient = new HttpClient(clientHandler);
+        }
 
-            var apiClient = new HttpClient(clientHandler);
-            var apiResponse = await apiClient.GetAsync("https://localhost:5001");
+        public void Dispose()
+        {
+            _output.WriteLine($"Disposing api client");
+            _apiClient.Dispose();
+        }
+
+        [Fact]
+        [Trait("Category", "Public")]
+        public async Task AllowAccessPublicData()
+        {
+            var apiResponse = await _apiClient.GetAsync("https://localhost:5001");
 
             Assert.True(apiResponse.IsSuccessStatusCode);
+            _output.WriteLine($"AllowAccessPublicData - Status code: {apiResponse.StatusCode}");
 
             var stringResponse = await apiResponse.Content.ReadAsStringAsync();
 
@@ -28,48 +45,36 @@ namespace JwtBearerExample.Tests
         }
 
         [Fact]
+        [Trait("Category", "Public")]
         public async Task BlockUnauthorizedAccessManagerData()
         {
-            HttpClientHandler clientHandler = new HttpClientHandler();
-            clientHandler.ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; };
-
-            var apiClient = new HttpClient(clientHandler);
-            var apiResponse = await apiClient.GetAsync("https://localhost:5001/content/manager");
+            var apiResponse = await _apiClient.GetAsync("https://localhost:5001/content/manager");
 
             Assert.True(apiResponse.StatusCode == HttpStatusCode.Unauthorized);
         }
 
         [Fact]
+        [Trait("Category", "Public")]
         public async Task BlockUnauthorizedAccessEmployeeData()
         {
-            HttpClientHandler clientHandler = new HttpClientHandler();
-            clientHandler.ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; };
-
-            var apiClient = new HttpClient(clientHandler);
-            var apiResponse = await apiClient.GetAsync("https://localhost:5001/content/employee");
+            var apiResponse = await _apiClient.GetAsync("https://localhost:5001/content/employee");
 
             Assert.True(apiResponse.StatusCode == HttpStatusCode.Unauthorized);
         }
 
         [Fact]
+        [Trait("Category", "Public")]
         public async Task BlockUnauthorizedAccessAuthenticatedData()
         {
-            HttpClientHandler clientHandler = new HttpClientHandler();
-            clientHandler.ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; };
-
-            var apiClient = new HttpClient(clientHandler);
-            var apiResponse = await apiClient.GetAsync("https://localhost:5001/content/authenticated");
+            var apiResponse = await _apiClient.GetAsync("https://localhost:5001/content/authenticated");
 
             Assert.True(apiResponse.StatusCode == HttpStatusCode.Unauthorized);
         }
 
         [Fact]
+        [Trait("Category", "Login")]
         public async Task RefuseInvalidUserPassword()
         {
-            HttpClientHandler clientHandler = new HttpClientHandler();
-            clientHandler.ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; };
-
-            var apiClient = new HttpClient(clientHandler);
             var loginData = new {
                 login = "user_1",
                 password = "65432"
@@ -77,18 +82,15 @@ namespace JwtBearerExample.Tests
 
             var content = new StringContent(JsonConvert.SerializeObject(loginData), Encoding.UTF8, "application/json");
 
-            var apiResponse = await apiClient.PostAsync("https://localhost:5001/user/login", content);
+            var apiResponse = await _apiClient.PostAsync("https://localhost:5001/user/login", content);
 
             Assert.True(apiResponse.StatusCode == HttpStatusCode.NotFound);
         }
 
         [Fact]
+        [Trait("Category", "Authorized")]
         public async Task LoginWithSuccess()
         {
-            HttpClientHandler clientHandler = new HttpClientHandler();
-            clientHandler.ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; };
-
-            var apiClient = new HttpClient(clientHandler);
             var loginData = new {
                 login = "user_1",
                 password = "654321"
@@ -96,18 +98,15 @@ namespace JwtBearerExample.Tests
 
             var content = new StringContent(JsonConvert.SerializeObject(loginData), Encoding.UTF8, "application/json");
 
-            var apiResponse = await apiClient.PostAsync("https://localhost:5001/user/login", content);
+            var apiResponse = await _apiClient.PostAsync("https://localhost:5001/user/login", content);
 
             Assert.True(apiResponse.IsSuccessStatusCode);
         }
 
         [Fact]
+        [Trait("Category", "Authorized")]
         public async Task EmployeeAccessPublicData()
         {
-            HttpClientHandler clientHandler = new HttpClientHandler();
-            clientHandler.ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; };
-
-            var apiClient = new HttpClient(clientHandler);
             var loginData = new {
                 login = "user_1",
                 password = "654321"
@@ -115,7 +114,7 @@ namespace JwtBearerExample.Tests
 
             var content = new StringContent(JsonConvert.SerializeObject(loginData), Encoding.UTF8, "application/json");
 
-            var apiResponse = await apiClient.PostAsync("https://localhost:5001/user/login", content);
+            var apiResponse = await _apiClient.PostAsync("https://localhost:5001/user/login", content);
 
             Assert.True(apiResponse.IsSuccessStatusCode);
 
@@ -124,20 +123,17 @@ namespace JwtBearerExample.Tests
             dynamic authData = JObject.Parse(response);
             string token =  (string)authData["token"];
 
-            apiClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
-            apiResponse = await apiClient.GetAsync("https://localhost:5001/content/public");
+            _apiClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            apiResponse = await _apiClient.GetAsync("https://localhost:5001/content/public");
 
             Assert.True(apiResponse.IsSuccessStatusCode);
 
         }
 
         [Fact]
+        [Trait("Category", "Authorized")]
         public async Task EmployeeAccessEmployeeData()
         {
-            HttpClientHandler clientHandler = new HttpClientHandler();
-            clientHandler.ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; };
-
-            var apiClient = new HttpClient(clientHandler);
             var loginData = new {
                 login = "user_1",
                 password = "654321"
@@ -145,7 +141,7 @@ namespace JwtBearerExample.Tests
 
             var content = new StringContent(JsonConvert.SerializeObject(loginData), Encoding.UTF8, "application/json");
 
-            var apiResponse = await apiClient.PostAsync("https://localhost:5001/user/login", content);
+            var apiResponse = await _apiClient.PostAsync("https://localhost:5001/user/login", content);
 
             Assert.True(apiResponse.IsSuccessStatusCode);
 
@@ -154,19 +150,16 @@ namespace JwtBearerExample.Tests
             dynamic authData = JObject.Parse(response);
             string token =  (string)authData["token"];
 
-            apiClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
-            apiResponse = await apiClient.GetAsync("https://localhost:5001/content/employee");
+            _apiClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            apiResponse = await _apiClient.GetAsync("https://localhost:5001/content/employee");
 
             Assert.True(apiResponse.IsSuccessStatusCode);
         }
 
         [Fact]
+        [Trait("Category", "Authorized")]
         public async Task EmployeeAccessManagerData()
         {
-            HttpClientHandler clientHandler = new HttpClientHandler();
-            clientHandler.ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; };
-
-            var apiClient = new HttpClient(clientHandler);
             var loginData = new {
                 login = "user_1",
                 password = "654321"
@@ -174,7 +167,7 @@ namespace JwtBearerExample.Tests
 
             var content = new StringContent(JsonConvert.SerializeObject(loginData), Encoding.UTF8, "application/json");
 
-            var apiResponse = await apiClient.PostAsync("https://localhost:5001/user/login", content);
+            var apiResponse = await _apiClient.PostAsync("https://localhost:5001/user/login", content);
 
             Assert.True(apiResponse.IsSuccessStatusCode);
 
@@ -183,18 +176,15 @@ namespace JwtBearerExample.Tests
             dynamic authData = JObject.Parse(response);
             string token =  (string)authData["token"];
 
-            apiClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
-            apiResponse = await apiClient.GetAsync("https://localhost:5001/content/manager");
+            _apiClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            apiResponse = await _apiClient.GetAsync("https://localhost:5001/content/manager");
 
             Assert.True(apiResponse.StatusCode == HttpStatusCode.Forbidden);
         }
         [Fact]
+        [Trait("Category", "Authorized")]
         public async Task EmployeeAccessAuthenticatedData()
         {
-            HttpClientHandler clientHandler = new HttpClientHandler();
-            clientHandler.ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; };
-
-            var apiClient = new HttpClient(clientHandler);
             var loginData = new {
                 login = "user_1",
                 password = "654321"
@@ -202,7 +192,7 @@ namespace JwtBearerExample.Tests
 
             var content = new StringContent(JsonConvert.SerializeObject(loginData), Encoding.UTF8, "application/json");
 
-            var apiResponse = await apiClient.PostAsync("https://localhost:5001/user/login", content);
+            var apiResponse = await _apiClient.PostAsync("https://localhost:5001/user/login", content);
 
             Assert.True(apiResponse.IsSuccessStatusCode);
 
@@ -211,10 +201,13 @@ namespace JwtBearerExample.Tests
             dynamic authData = JObject.Parse(response);
             string token =  (string)authData["token"];
 
-            apiClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
-            apiResponse = await apiClient.GetAsync("https://localhost:5001/content/authenticated");
+            _apiClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            apiResponse = await _apiClient.GetAsync("https://localhost:5001/content/authenticated");
 
             Assert.True(apiResponse.IsSuccessStatusCode);
         }
+
+        // Test multiple logins
+
     }
 }
